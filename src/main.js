9ff1a52a0beda0b6eedf5da0a299c00ef8535ba5
YoanMln <yoan.moulin@hotmail.fr>
2025-12-03 14:55:32 +0100

    Add home path

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from "./App.vue";
 import router from "./router/router";
 import "./style.scss";
 
-createApp(App).mount("#app");
-
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import citiesList from "@/views/citiesList.vue";
+import home from "@/views/home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "cityList",
+      name: "home",
+      component: home,
+    },
+    {
+      path: "/villes",
+      name: "citiesList",
       component: citiesList,
     },
   ],
