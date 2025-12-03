fd15faf6880c92d44ce309aad84800d2e19e8e2b
YoanMln <yoan.moulin@hotmail.fr>
2025-12-03 16:13:12 +0100

    Add JS object city

---
+++ src/components/city.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { reactive } from "vue";
+
+const city = reactive({
+  name: "Annecy",
+  weather: "Nuageux",
+  temperature: "8 degrés",
+  updateAt: new Date(),
+});
+</script>
+
+<template>
+  <h1>{{ city.name }}</h1>
+  <p>Météo {{ city.weather }}</p>
+  <p>Température {{ city.temperature }}</p>
+  <p>MAJ {{ city.updateAt.toLocaleString() }}</p>
+</template>
+
+<style scoped lang="scss"></style>
