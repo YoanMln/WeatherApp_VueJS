1a787335fb2375706c267adfd10e0af13629e13f
YoanMln <yoan.moulin@hotmail.fr>
2025-12-04 10:21:57 +0100

    Config scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
-import "./style.scss";
+import "../main.scss"
 
 const app = createApp(App);
 app.use(router);
