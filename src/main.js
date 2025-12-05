c8db3d38835f6ee80e513719c4d9a05db0557b5d
YoanMln <yoan.moulin@hotmail.fr>
2025-12-05 09:21:10 +0100

    Add vue-leaflet package

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
-import "../main.scss"
+import "../main.scss";
+import "leaflet/dist/leaflet.css";
 
 const app = createApp(App);
 app.use(router);
