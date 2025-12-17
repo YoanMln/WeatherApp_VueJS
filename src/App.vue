a66bfe41eb00ed20c6f81f1e8e580243e49f2aba
YoanMln <yoan.moulin@hotmail.fr>
2025-12-17 09:28:09 +0100

    Dynamic weather background

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,96 @@
 <script setup>
 import { RouterView } from "vue-router";
+import { ref, onMounted, computed } from "vue";
+import { getWeatherByCoords } from "./services/weatherService";
+
+const weatherMain = ref(null);
+
+
+onMounted(() => {
+  if (!navigator.geolocation) {
+    weatherMain.value = "default";
+    
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    async (position) => {
+      const { latitude, longitude } = position.coords;
+
+      try {
+        const data = await getWeatherByCoords(latitude, longitude);
+        weatherMain.value = data.weather[0].main;
+      } catch (err) {
+        console.log("erreur background météo", err);
+        weatherMain.value = "default";
+      } finally {
+       
+      }
+    },
+    () => {
+      weatherMain.value = "default";
+    
+    }
+  );
+});
+
+const weatherClass = computed(() => {
+  if (!weatherMain.value) return "bg-none";
+
+  const map = {
+    Clear: "bg-clear",
+    Clouds: "bg-clouds",
+    Rain: "bg-rain",
+    Snow: "bg-snow",
+    Thunderstorm: "bg-storm",
+    default: "bg-default",
+  };
+  return map[weatherMain.value] || "bg-default";
+});
 </script>
 
 <template>
-  <div class="app">
-     
-    <nav>
-      <router-link to="/">Accueil</router-link>
-      <router-link to="/villes">Liste des Villes</router-link>
-      <router-link to="/ville">Ville</router-link>
-    </nav>
+  <div id="app" :class="weatherClass">
+    <div class="app">
+      <div v-if="isLoading" class="spinner">
+        <div class="spinner-circle"></div>
+        <p>Chargement en cours</p>
+      </div>
+      <nav v-else>
+        <router-link to="/">Accueil</router-link>
+        <router-link to="/villes">Liste des Villes</router-link>
+        <router-link to="/ville">Ville</router-link>
+      </nav>
 
-    <RouterView></RouterView>
+      <RouterView></RouterView>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
+#app {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+.app {
+  height: 100vh;
+  overflow-y: auto;
+}
+
 .title {
   text-align: center;
 }
+
 nav {
   display: flex;
   justify-content: space-around;
   margin-top: 3rem;
 }
+
+
+
 </style>
